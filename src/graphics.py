793442793c7ce378eeeca278688f45b773eea2cd
Kalle Luopajärvi <kalle.luopajarvi@helsinki.fi>
2021-04-01 02:20:32 +0300

    Modify PolylineGraphic
        Change to use the Polyline class

--- src/graphics.py
+++ src/graphics.py
@@ -28,11 +28,9 @@ class Graphic(ABC):
         pass
 
 class PolylineGraphic(Graphic):
-    def __init__(self, points, color):
-        self._points = points
+    def __init__(self, polyline, color):
+        self._polyline = polyline
         self.color = color
-        self._location = (0, 0)
-        self._rotation = 0
 
     def draw(self, surface, offset=(0, 0)):
         '''Draws image on `surface`.
@@ -41,37 +39,26 @@ class PolylineGraphic(Graphic):
 
         dirty_rects = []
         # TODO draw only visible if necessary
-        for i in range(len(self._points)-1):
-            start = self._transform_point(self._points[i])
-            end = self._transform_point(self._points[i+1])
-            dirty_rects.append(pygame.draw.aaline(surface, self.color, start,
-                                                 end))
+        for line in self._polyline.lines:
+            dirty_rects.append(
+                pygame.draw.aaline(surface, self.color, line.begin, line.end))
         return dirty_rects
 
     @property
     def location(self):
-        return self._location
+        return self._polyline.location
 
     @location.setter
     def location(self, value):
-        self._location = value
-        for i in range(len(self._points)):
-            self._points[i][0] += self._location[0]
-            self._points[i][1] += self._location[1]
+        self._polyline.location = value
 
     @property
     def rotation(self):
-        return self._rotation
+        return self._polyline.rotation
 
     @rotation.setter
     def rotation(self, value):
-        self._rotation = value
-
-    def _transform_point(self, point):
-        vec = Vector2(point)
-        vec = vec.rotate_rad(self._rotation)
-        vec += Vector2(self._location)
-        return (vec[0], vec[1])
+        self._polyline.rotation = value
 
 class ImageGraphic(Graphic):
     def __init__(self, location, image_path):
