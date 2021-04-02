e099b63d1219ac0f052b69205225537805a70d7b
Kalle Luopajärvi <kalle.luopajarvi@helsinki.fi>
2021-04-03 02:49:47 +0300

    Add collision checks to GameState
        Add Player class to manage the spawning of new
        planes after the plane has been destroyed.

--- src/game.py
+++ src/game.py
@@ -1,43 +1,80 @@
 from pygame import Rect
+from pygame import Vector2
 import logging
 import graphics
 from pathlib import Path
 import pygame
 import time
-class GameInput:
-    def __init__(self, event_handler):
-        self.event_handler = event_handler
-        self.keymaps = {}
-        self.should_quit = False
+import itertools
 
-    def bind_keys(self, new_keymaps):
-        for keycode in new_keymaps:
-            if keycode in self.keymaps:
-                raise Exception('Key already bound to a function')
-            self.keymaps[keycode] = new_keymaps[keycode]
+class Player:
+    def __init__(self, plane_factory, player_input):
+        self.plane_factory = plane_factory
+        self.player_input = player_input
+        self.plane = None
 
-    def handle_inputs(self):
-        self.should_quit = False
-        for event in self.event_handler.get_events():
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                self.should_quit = True
+        self.since_last_plane = 0
+        self.plane_interval = 2
 
-        pressed = self.event_handler.get_pressed()
+        self._new_objects = []
+        self.player_input.bind_shoot(self._start_new_flight)
 
-        for keycode in self.keymaps:
-            if pressed[keycode]:
-                self.keymaps[keycode]()
+    def _start_new_flight(self):
+        self.player_input.clear_shoot()
+        self.plane = self.plane_factory.plane(self.player_input)
+        self._new_objects.append(self.plane)
+
+    def update(self, delta_time):
+        self.since_last_plane += delta_time
+        if self.plane == None:
+            if self.since_last_plane > self.plane_interval:
+                self.player_input.bind_shoot(self._start_new_flight)
+            return
+
+        if not self.plane.alive:
+            self.plane = None
+            self.since_last_plane = 0
+
+    def new_objects(self):
+        tmp = self._new_objects
+        self._new_objects = []
+        return tmp
 
 class GameState:
-    def __init__(self, game_objects):
+    def __init__(self, game_objects, players):
         self.game_objects = game_objects
+        self.players = players
+        self._update_game_object_list()
 
     def run_tick(self, delta_time):
-        self._update_objects(delta_time)
+        self._update_players(delta_time)
+        self._update_game_objects(delta_time)
+        self._handle_collisions()
+        self._update_game_object_list()
 
-    def _update_objects(self, delta_time):
+    def _update_players(self, delta_time):
+        for player in self.players:
+            player.update(delta_time)
+
+    def _update_game_objects(self, delta_time):
         for game_object in self.game_objects:
             game_object.update(delta_time)
+
+    def _handle_collisions(self):
+        for x, y in itertools.combinations(self.game_objects, 2):
+            if x.shape.intersects(y.shape):
+                x.damage(y.collision_damage)
+                y.damage(x.collision_damage)
+
+    def _update_game_object_list(self):
+        new_game_objects = []
+        for game_object in self.game_objects:
+            new_game_objects.extend(game_object.new_objects())
+
+        for player in self.players:
+            new_game_objects.extend(player.new_objects())
+
+        self.game_objects = new_game_objects
 
 def sleep_until(until):
     if time.time() > until:
