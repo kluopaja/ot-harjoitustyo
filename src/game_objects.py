3b7aa31ad890bcba4080fbe064f1494b19e025fc
Kalle Luopajärvi <kalle.luopajarvi@helsinki.fi>
2021-04-06 17:04:04 +0300

    Refactor Plane
        Separate plane controlling and physics related
        calculations to separate classes.
    
        Use the decorator pattern to construct the plane behaviour from
        from various parts.

--- src/game_objects.py
+++ src/game_objects.py
@@ -4,6 +4,7 @@ from constants import EPS
 from pygame import Vector2
 from graphics import ImageGraphic
 from shapes import Rectangle
+from physics import BasePhysics, GravityPhysics, BodyPhysics, WingPhysics, angle_between, PhysicsController
 
 class PlaneFactory:
     def __init__(self, file_path):
@@ -22,9 +23,14 @@ class PlaneFactory:
         image_graphic = ImageGraphic.from_image_path(self.file_path,
                                                      Vector2(0, 0), self.size)
         rectangle = self._plane_rectangle(self.size)
-        plane = Plane(rectangle, image_graphic, max_acceleration=self.acceleration,
-                      max_rotation=self.rotation, body_drag=self.body_drag,
-                      wing_size=self.wing_size, gravity=self.gravity,
+
+        tmp = BasePhysics(Vector2(self.start_position), Vector2(0, 0), Vector2(1, 0))
+        tmp = GravityPhysics(tmp, self.gravity)
+        tmp = BodyPhysics(tmp, self.body_drag)
+        tmp = WingPhysics(tmp, self.wing_size)
+        plane_physics = PhysicsController(tmp, self.acceleration, self.rotation)
+
+        plane = Plane(rectangle, image_graphic, plane_physics,
                       health=self.health, collision_damage=self.collision_damage)
         player_input.bind_to_plane(plane)
         print("new_plane")
@@ -35,24 +41,15 @@ class PlaneFactory:
         return Rectangle(Vector2(-size[0]/2, -size[1]/2),
                          Vector2(size[0]/2, -size[1]/2),
                          Vector2(-size[0]/2, size[1]/2))
-
-
 class Plane:
     '''
     For lift and drag see http://www.aerospaceweb.org/question/airfoils/q0150b.shtml
     '''
-    def __init__(self, shape, graphic, max_acceleration=4, max_rotation=5,
-                 body_drag=0.01, wing_size=0.3, gravity=5, health=100,
+    def __init__(self, shape, graphic, plane_physics, health=100,
                  collision_damage=100):
         self.graphic = graphic
         self.shape = shape
-        self.max_acceleration = max_acceleration
-        self.max_rotation = max_rotation
-        self.body_drag = body_drag
-        self.wing_size = wing_size
-
-        self.gravity = gravity
-
+        self.plane_physics = plane_physics
         self.health = health
         self.alive = True
 
@@ -62,21 +59,17 @@ class Plane:
         self.velocity = Vector2(0)
         self.front = Vector2(1.0, 0.0)
 
-        self._next_rotation = 0
-        self._next_acceleration = 0
-
     def up(self):
-        self._next_rotation = self.max_rotation
+        self.plane_physics.up()
 
     def down(self):
-        self._next_rotation = -self.max_rotation
+        self.plane_physics.down()
 
     def accelerate(self):
-        self._next_acceleration = self.max_acceleration
+        self.plane_physics.accelerate()
 
     def damage(self, amount):
         self.health -= amount
-
 
     def update(self, delta_time):
         if self.health <= 0:
@@ -85,21 +78,17 @@ class Plane:
         if not self.alive:
             return
 
-        new_front = self._new_front(delta_time)
-        new_velocity = self._new_velocity(delta_time)
-        self.front = new_front
-        self.velocity = new_velocity
+        self.plane_physics.update(delta_time)
 
-        self.location += self.velocity
+        self.location = Vector2(self.plane_physics.location)
+        self.rotation = -math.radians(self.plane_physics.front.as_polar()[1])
 
-        self._next_rotation = 0
-        self._next_acceleration = 0
+        self.graphic.location = Vector2(self.plane_physics.location)
+        self.graphic.rotation = -math.radians(self.plane_physics.front.as_polar()[1])
 
-        self.graphic.location = Vector2(self.location)
-        self.graphic.rotation = -math.radians(self.front.as_polar()[1])
+        self.shape.location = Vector2(self.plane_physics.location)
+        self.shape.rotation = -math.radians(self.plane_physics.front.as_polar()[1])
 
-        self.shape.location = Vector2(self.location)
-        self.shape.rotation = -math.radians(self.front.as_polar()[1])
 
     def new_objects(self):
         if not self.alive:
@@ -107,64 +96,6 @@ class Plane:
 
         return [self]
 
-    def _new_front(self, delta_time):
-        return self.front.rotate(-delta_time * math.degrees(self._next_rotation))
-
-    def _new_velocity(self, delta_time):
-        d_velocity = Vector2(0, 0)
-        d_velocity += self._calculate_body_drag()
-
-        angle_of_attack = self._angle_of_attack()
-        d_velocity += self._calculate_wing_drag(angle_of_attack)
-        d_velocity += self._calculate_wing_lift(angle_of_attack)
-
-        d_velocity += self._calculate_thrust()
-        d_velocity += self._calculate_gravity()
-
-        logging.debug(f"lift: {self._calculate_wing_lift(angle_of_attack)}")
-        logging.debug(f"drag: {self._calculate_wing_drag(angle_of_attack)}")
-        logging.debug(f"rotation: {self.front.as_polar()[1]}")
-        logging.debug(f"angle_of_attack: {angle_of_attack}")
-
-        return self.velocity + delta_time * d_velocity
-
-    def _angle_of_attack(self):
-        return angle_between(self.velocity, self.front)
-
-    def _calculate_thrust(self):
-        return self._next_acceleration * self.front
-
-    def _calculate_body_drag(self):
-        return -self.body_drag * self.velocity.magnitude() * self.velocity
-
-    def _calculate_wing_drag(self, angle_of_attack):
-        return -self.wing_size * (1 - math.cos(2*angle_of_attack)) \
-               * self.velocity.magnitude() * self.velocity
-
-    def _calculate_wing_lift(self, angle_of_attack):
-        lift_coefficient = 0
-        angle_mod = angle_of_attack
-        if angle_mod < 0:
-            angle_mod += math.pi
-
-        if angle_mod < math.pi/9:
-            lift_coefficient = angle_mod * 9 / math.pi
-        elif angle_mod > math.pi - math.pi/9:
-            lift_coefficient = -(math.pi - angle_mod) * 9 / math.pi
-        else:
-            lift_coefficient = math.sin(2*angle_mod)
-
-        logging.debug(f'lift_coef {lift_coefficient}')
-        logging.debug(f'lift_velo: {self._lift_velocity()}')
-
-        return self.wing_size * lift_coefficient \
-               * self.velocity.magnitude() * self._lift_velocity()
-
-    def _calculate_gravity(self):
-        return self.gravity * Vector2(0, 1)
-
-    def _lift_velocity(self):
-        return self.velocity.rotate_rad(-math.pi/2)
 
 class Ground:
     def __init__(self, shape, graphic):
@@ -180,27 +111,3 @@ class Ground:
 
     def damage(self, amount):
         pass
-
-def angle_between(start, end):
-    '''Angle from `start` end `end`.
-
-        Parameters:
-            `start` and `end` should be pygame.Vector2
-
-        Returns:
-            `result`, the angle in radians.
-
-            `result` is always in [-pi, pi]
-
-        Note:
-            Returns 0.0, if `start` or `end` are smaller than EPS
-    '''
-    if start.magnitude() < EPS or end.magnitude() < EPS:
-        return 0.0
-
-    result = math.fmod(math.radians(end.angle_to(start)), 2*math.pi)
-    result = math.fmod(result + 2*math.pi, 2*math.pi)
-    if result > math.pi:
-        result -= 2*math.pi
-    return result
-
---
+++ src/physics.py
@@ -0,0 +1,170 @@
+import math
+from pygame import Vector2
+from constants import EPS
+
+class BasePhysics:
+    def __init__(self, location, velocity, front):
+        self.location = location
+        self.velocity = velocity
+        self.front = front
+        self.acceleration = Vector2(0)
+
+    def update(self, delta_time):
+        self.velocity += delta_time * self.acceleration
+        self.acceleration = Vector2(0)
+        # TODO add delta_time to this!
+        self.location += self.velocity
+
+class PhysicsDecorator:
+    def __init__(self, physics):
+        self.physics = physics
+
+    def update(self, delta_time):
+        self.physics.update(delta_time)
+
+    @property
+    def location(self):
+        return self.physics.location
+
+    @location.setter
+    def location(self, value):
+        self.physics.location = value
+
+    @property
+    def velocity(self):
+        return self.physics.velocity
+
+    @velocity.setter
+    def velocity(self, value):
+        self.physics.velocity = value
+
+    @property
+    def front(self):
+        return self.physics.front
+
+    @front.setter
+    def front(self, value):
+        self.physics.front = value
+
+    @property
+    def acceleration(self):
+        return self.physics.acceleration
+
+    @acceleration.setter
+    def acceleration(self, value):
+        self.physics.acceleration = value
+
+class GravityPhysics(PhysicsDecorator):
+    def __init__(self, physics, gravity):
+        super().__init__(physics)
+        self.gravity = gravity
+
+    def update(self, delta_time):
+        self.acceleration += Vector2(0, 1) * self.gravity
+        self.physics.update(delta_time)
+
+class BodyPhysics(PhysicsDecorator):
+    def __init__(self, physics, body_drag):
+        super().__init__(physics)
+        self.body_drag = body_drag
+
+    def update(self, delta_time):
+        self.acceleration += -self.body_drag * self.velocity.magnitude() * self.velocity
+        self.physics.update(delta_time)
+
+class WingPhysics(PhysicsDecorator):
+    def __init__(self, physics, wing_size):
+        super().__init__(physics)
+        self.wing_size = wing_size
+
+    def update(self, delta_time):
+        self.acceleration += self._acceleration()
+        self.physics.update(delta_time)
+
+    def _acceleration(self):
+        result = Vector2(0)
+        angle_of_attack = self._angle_of_attack()
+        result += self._calculate_wing_drag(angle_of_attack) 
+        result += self._calculate_wing_lift(angle_of_attack)
+        return result
+
+    def _angle_of_attack(self):
+        return angle_between(self.velocity, self.front)
+
+    def _calculate_wing_drag(self, angle_of_attack):
+        return -self.wing_size * (1 - math.cos(2*angle_of_attack)) \
+               * self.velocity.magnitude() * self.velocity
+
+    def _calculate_wing_lift(self, angle_of_attack):
+        lift_coefficient = 0
+        angle_mod = angle_of_attack
+        if angle_mod < 0:
+            angle_mod += math.pi
+
+        if angle_mod < math.pi/9:
+            lift_coefficient = angle_mod * 9 / math.pi
+        elif angle_mod > math.pi - math.pi/9:
+            lift_coefficient = -(math.pi - angle_mod) * 9 / math.pi
+        else:
+            lift_coefficient = math.sin(2*angle_mod)
+
+        return self.wing_size * lift_coefficient \
+               * self.velocity.magnitude() * self._lift_velocity()
+
+    def _lift_velocity(self):
+        return self.velocity.rotate_rad(-math.pi/2)
+
+class PhysicsController(PhysicsDecorator):
+    def __init__(self, physics, max_acceleration, max_rotation):
+        super().__init__(physics)
+        self.max_acceleration = max_acceleration
+        self.max_rotation = max_rotation
+        self._next_rotation = 0.0
+        self._next_acceleration = 0.0
+
+    def up(self):
+        self._next_rotation = self.max_rotation
+
+    def down(self):
+        self._next_rotation = -self.max_rotation
+
+    def accelerate(self):
+        self._next_acceleration = self.max_acceleration
+
+    def update(self, delta_time):
+        self.front = self._new_front(delta_time)
+        self.acceleration += self._calculate_thrust()
+        self._next_rotation = 0.0
+        self._next_acceleration = 0.0
+        self.physics.update(delta_time)
+
+    def _new_front(self, delta_time):
+        return self.front.rotate(-delta_time * math.degrees(self._next_rotation))
+
+    def _calculate_thrust(self):
+        return self._next_acceleration * self.front
+
+
+def angle_between(start, end):
+    '''Angle from `start` end `end`.
+
+        Parameters:
+            `start` and `end` should be pygame.Vector2
+
+        Returns:
+            `result`, the angle in radians.
+
+            `result` is always in [-pi, pi]
+
+        Note:
+            Returns 0.0, if `start` or `end` are smaller than EPS
+    '''
+    if start.magnitude() < EPS or end.magnitude() < EPS:
+        return 0.0
+
+    result = math.fmod(math.radians(end.angle_to(start)), 2*math.pi)
+    result = math.fmod(result + 2*math.pi, 2*math.pi)
+    if result > math.pi:
+        result -= 2*math.pi
+    return result
+
