318276cd6c57caac49b79ba7aa9d0d6fc6d42246
Kalle Luopajärvi <kalle.luopajarvi@helsinki.fi>
2021-04-27 20:44:57 +0300

    Fix problem with older python versions
        sqlite3.connect requires str on older python versions

--- src/database_connection.py
+++ src/database_connection.py
@@ -1,7 +1,7 @@
 import sqlite3
 from config import DATABASE_PATH
 
-connection = sqlite3.connect(DATABASE_PATH)
+connection = sqlite3.connect(str(DATABASE_PATH))
 connection.row_factory = sqlite3.Row
 
 
