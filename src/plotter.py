226538a178f40edaff755b324fa039350b46ca84
Kalle Luopajärvi <kalle.luopajarvi@helsinki.fi>
2021-04-27 17:56:01 +0300

    Add code for plotting histograms
        Use matplotlib, pandas and numpy to draw the histograms into
        a numpy array which can then be drawn to pygame surface.
    
        Update project dependencies

---
+++ src/plotter.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+class Plotter:
+    def __init__(self):
+        self.figure = plt.figure(frameon = False)
+
+    def plot_histogram_to_image(self, data, x, hue, bin_range,
+                                bins=10, title="", width=100, height=100):
+        """
+
+        Args:
+            `data`: a dataframe
+            `x`: a string
+                The name of the column to be plotted
+            `hue`: a string
+                The name of the column specifying the groups
+            `bin_range`: a tuple of scalars
+                The range of the histogram
+            `bins`: an integer
+                The number of bins in the histogram
+            `title`: a string
+                The title of the plot
+            `width`: an integer
+                The width of the plot in pixels
+            `height`: an integer
+                The height of the plot in pixels
+
+        Returns:
+            `image`: a (width, height, 4) numpy array
+                The image of the plot
+        """
+        plt.ioff()
+        ax = self.figure.gca()
+        ax.patch.set_visible(False)
+        self.figure.patch.set_visible(False)
+
+        sns.histplot(data, x=x, hue=hue, binrange = bin_range, 
+                     bins=bins, element="poly", fill=False, ax=ax)
+
+        ax.set_title(title)
+
+        dpi = self.figure.get_dpi()
+
+        # add small value for numerical errors
+        self.figure.set_size_inches(width/float(dpi) + 1e-5, height/float(dpi) + 1e-5)
+
+        self.figure.canvas.draw()
+
+        image = np.fromstring(self.figure.canvas.tostring_argb(), dtype=np.uint8, sep='')
+        image = image.reshape(height, width, 4)
+        image = np.swapaxes(image, 0, 1)
+        self.figure.clf()
+        return image
+
