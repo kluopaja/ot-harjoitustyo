7f9ad9753196b16e0307912323ca680a4cf8120f
Kalle Luopajärvi <kalle.luopajarvi@helsinki.fi>
2021-05-03 17:57:41 +0300

    Add exception handling to matplotlib plotting
        The matplotlib related code seemed to fail when tried on macOS.
    
        For now, fix this by putting the code inside try: ... except

--- src/plotter.py
+++ src/plotter.py
@@ -31,26 +31,29 @@ class Plotter:
             `image`: a (width, height, 4) numpy array
                 The image of the plot
         """
-        plt.ioff()
-        ax = self.figure.gca()
-        ax.patch.set_visible(False)
-        self.figure.patch.set_visible(False)
+        try:
+            plt.ioff()
+            ax = self.figure.gca()
+            ax.patch.set_visible(False)
+            self.figure.patch.set_visible(False)
 
-        sns.histplot(data, x=x, hue=hue, binrange = bin_range, 
-                     bins=bins, element="poly", fill=False, ax=ax)
+            sns.histplot(data, x=x, hue=hue, binrange = bin_range,
+                         bins=bins, element="poly", fill=False, ax=ax)
 
-        ax.set_title(title)
+            ax.set_title(title)
 
-        dpi = self.figure.get_dpi()
+            dpi = self.figure.get_dpi()
 
-        # add small value for numerical errors
-        self.figure.set_size_inches(width/float(dpi) + 1e-5, height/float(dpi) + 1e-5)
+            # add small value for numerical errors
+            self.figure.set_size_inches(width/float(dpi) + 1e-5, height/float(dpi) + 1e-5)
 
-        self.figure.canvas.draw()
+            self.figure.canvas.draw()
 
-        image = np.fromstring(self.figure.canvas.tostring_argb(), dtype=np.uint8, sep='')
-        image = image.reshape(height, width, 4)
-        image = np.swapaxes(image, 0, 1)
-        self.figure.clf()
-        return image
-
+            image = np.fromstring(self.figure.canvas.tostring_argb(), dtype=np.uint8, sep='')
+            image = image.reshape(height, width, 4)
+            image = np.swapaxes(image, 0, 1)
+            self.figure.clf()
+            return image
+        except Exception:
+            # the plotting seemed to fail on some macOS versions.
+            return np.zeros(shape=(width, height, 4))
