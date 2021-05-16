dfe4be88829107de21ba3c4bdfd7c6e4d153d558
Kalle Luopajärvi <kalle.luopajarvi@helsinki.fi>
2021-05-16 19:53:51 +0300

    Add config file validation
        Validate config files with jsonschema
    
        Add schemas to assets/schemas/
    
        Add dependency to jsonschema

--- src/config.py
+++ src/config.py
@@ -10,26 +10,50 @@ os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
 from pygame import Vector2
 import pygame
 
+import jsonschema
+
 from graphics.graphics import PolylineGraphic
 from game.shapes import Polyline
 from game.game_objects import Ground
 
 
 project_root = Path(__file__).parent.parent
+SCHEMAS_PATH = project_root / "assets/schemas"
 CONFIG_PATH = project_root / "assets/config.json"
 DEFAULT_DATABASE_PATH = project_root / "data/database.sqlite"
+
+class ValidationError(Exception):
+    pass
+
+
+def validate_keys_config(keys_config_path):
+    schema = json.load(open(SCHEMAS_PATH / "keys_schema.json"))
+    data = json.load(open(keys_config_path, "r"))
+    try:
+        jsonschema.validate(instance=data, schema=schema)
+    except jsonschema.exceptions.ValidationError as ex:
+        msg = str(ex)
+        raise ValidationError("Error parsing keys config:\n" + msg)
 
 class Config:
     """A class for storing all of the configs."""
     def __init__(self, file_path):
         try:
             file_dir = file_path.parent
+            schema = json.load(open(SCHEMAS_PATH / "config_schema.json"))
             data = json.load(open(file_path, "r"))
+            try:
+                jsonschema.validate(instance=data, schema=schema)
+            except jsonschema.exceptions.ValidationError as ex:
+                msg = str(ex)
+                raise ValidationError("Error parsing main config:\n" + msg)
+
             self.level_config_selector = LevelConfigSelector(file_dir / data["level_dir"])
             self.plane_config = PlaneConfig(file_dir / data["plane_config_path"])
             self.background_config = BackgroundConfig(file_dir / data["background_config_path"])
 
             keys_config_path = file_dir / data["keys_config_path"]
+            validate_keys_config(keys_config_path)
             self.player_input_configs = player_input_configs_loader(keys_config_path)
             self.game_input_config = GameInputConfig.from_file(keys_config_path)
             self.menu_input_config = MenuInputConfig.from_file(keys_config_path)
@@ -64,32 +88,27 @@ class Config:
 
             self.game_fps = data["game_fps"]
 
+        except ValidationError as ex:
+            logging.critical(ex)
+            sys.exit()
         except Exception:
             logging.critical(traceback.format_exc())
             logging.critical("Failed parsing configuration files")
             sys.exit()
 
-        self._validate_config()
-
-    def _validate_config(self):
-        if self.window_width < 100 or self.window_height < 100:
-            logging.critical("The window height and width should both be at least 100")
-            sys.exit()
-
-        if self.font_size > 0.5:
-            logging.critical("The maximum font size is 0.5")
-            sys.exit()
-
-
-
 class LevelConfig:
     """A class for storing the properties of a level."""
 
     def __init__(self, file_path):
+        schema = json.load(open(SCHEMAS_PATH / "level_schema.json"))
         self._data = json.load(open(file_path, "r"))
-        if ("starting_locations" not in self._data) or len(self._data["starting_locations"]) == 0:
-            raise ValueError(
-                f"Error in loading {file_path}: no starting locations provided.")
+
+        try:
+            jsonschema.validate(instance=self._data, schema=schema)
+        except jsonschema.exceptions.ValidationError as ex:
+            msg = str(ex)
+            raise ValidationError(
+                f"Error parsing level config {file_path}:\n" + msg)
 
         background_path = file_path.parent / self._data["background_config_path"]
         self.background_config = BackgroundConfig(background_path)
@@ -134,6 +153,7 @@ class LevelConfigSelector:
         if len(self.level_configs) == 0:
             raise ValueError("No level configs could be found!")
 
+
     def get_selected(self):
         """Returns a LevelConfig object for the currently selected level"""
         return self.level_configs[self.selected_idx]
@@ -162,7 +182,14 @@ class LevelConfigSelector:
 
 class BackgroundConfig:
     def __init__(self, config_path):
+        schema = json.load(open(SCHEMAS_PATH / "background_schema.json"))
         data = json.load(open(config_path, "r"))
+        try:
+            jsonschema.validate(instance=data, schema=schema)
+        except jsonschema.exceptions.ValidationError as ex:
+            msg = str(ex)
+            raise ValidationError("Error parsing background config:\n" + msg)
+
         self.image_file_path = config_path.parent / data["image_file_path"]
         self.image_size = (data["image_size"]["width"], data["image_size"]["height"])
         self.n_images = data["n_images"]
@@ -203,7 +230,6 @@ class MenuInputConfig:
             pygame_key_code(keys["accept"]),
             pygame_key_code(keys["erase"]))
 
-
 class PlayerInputConfig:
     """A class for storing the keys for a single player.
 
@@ -243,7 +269,13 @@ def player_input_configs_loader(keymaps_file_path):
 class PlaneConfig:
     """A class for reading and storing the properties of a plane."""
     def __init__(self, plane_config_path):
+        schema = json.load(open(SCHEMAS_PATH / "plane_schema.json"))
         data = json.load(open(plane_config_path, "r"))
+        try:
+            jsonschema.validate(instance=data, schema=schema)
+        except jsonschema.exceptions.ValidationError as ex:
+            msg = str(ex)
+            raise ValidationError("Error parsing plane config:\n" + msg)
         self.image_file_path = plane_config_path.parent / data["image_file_path"]
         self.gun_config = GunConfig(plane_config_path.parent / data["gun_config_path"])
         self.size = Vector2(data["size"]["width"], data["size"]["height"])
@@ -261,7 +293,13 @@ class PlaneConfig:
 class GunConfig:
     """A class for reading and storing the properties of a gun."""
     def __init__(self, gun_config_path):
+        schema = json.load(open(SCHEMAS_PATH / "gun_schema.json"))
         data = json.load(open(gun_config_path, "r"))
+        try:
+            jsonschema.validate(instance=data, schema=schema)
+        except jsonschema.exceptions.ValidationError as ex:
+            msg = str(ex)
+            raise ValidationError("Error parsing gun config:\n" + msg)
         self.bullet_config = BulletConfig(
             gun_config_path.parent / data["bullet_config_path"])
         self.bullet_spawn_time = data["bullet_spawn_time"]
@@ -272,7 +310,13 @@ class GunConfig:
 class BulletConfig:
     """A class for reading and storing the properties of a bullet."""
     def __init__(self, bullet_config_path):
+        schema = json.load(open(SCHEMAS_PATH / "bullet_schema.json"))
         data = json.load(open(bullet_config_path, "r"))
+        try:
+            jsonschema.validate(instance=data, schema=schema)
+        except jsonschema.exceptions.ValidationError as ex:
+            msg = str(ex)
+            raise ValidationError("Error parsing bullet config:\n" + msg)
         self.image_file_path = bullet_config_path.parent / data["image_file_path"]
         self.diameter = data["diameter"]
         self.gravity = data["gravity"]
